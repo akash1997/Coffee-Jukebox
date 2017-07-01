1c4f9f205fc63e619d49e40470d46a2bd7c1fd8f
Akash Agrawal <akashrocksha@gmail.com>
2017-07-02 02:49:23 +0530

    user feedback ready app

--- app/src/server.js
+++ app/src/server.js
@@ -1,11 +1,42 @@
 var express = require('express');
 var app = express();
+var root = process.cwd();
 
-//your routes here
+app.get('/songs/:name', function (req, res) {
+	var name = req.params.name;
+	var loc = "/songs/"+name;
+	console.log(loc);
+    res.sendFile(loc, {root});
+});
+
 app.get('/', function (req, res) {
-    res.send("Hello World!");
+    res.sendFile('myown1.html', {root});
+});
+
+app.get('/songList', function (req, res) {
+    res.sendFile('myown2.html', {root});
+});
+
+app.get('/adminlogin', function (req, res) {
+    res.sendFile('login_page.html', {root});
+});
+
+app.get('/adminconsole', function (req, res) {
+    res.sendFile('myown3.html', {root});
+});
+
+app.get('/songrequest', function (req, res) {
+    res.sendFile('songRequest.html', {root});
+});
+
+app.get('/player', function (req, res) {
+    res.sendFile('songPlay.html', {root});
+});
+
+app.get('/js.cookie.js', function (req, res) {
+    res.sendFile('js.cookie.js', {root});
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Coffee-Jukebox app listening on port 8080!');
 });
