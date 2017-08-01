865e6319bc35a3c884d9dacdafbd8fe989fa7f08
Akash Agrawal <akashrocksha@gmail.com>
2017-08-01 12:36:00 +0530

    Removed unnecessary logs on console

--- app/src/server.js
+++ app/src/server.js
@@ -5,28 +5,24 @@ var root = process.cwd();
 app.get('/scripts/:scriptname', function (req, res) {
 	var scriptName = req.params.scriptname;
 	var loc = "/scripts/"+scriptName;
-	console.log(loc);
     res.sendFile(loc, {root});
 });
 
 app.get('/stylesheets/:cssname', function (req, res) {
 	var cssName = req.params.cssname;
 	var loc = "/stylesheets/"+cssName;
-	console.log(loc);
     res.sendFile(loc, {root});
 });
 
 app.get('/images/:imagename', function (req, res) {
 	var imageName = req.params.imagename;
 	var loc = "/images/"+imageName;
-	console.log(loc);
     res.sendFile(loc, {root});
 });
 
 app.get('/songs/:name', function (req, res) {
 	var name = req.params.name;
 	var loc = "/songs/"+name;
-	console.log(loc);
     res.sendFile(loc, {root});
 });
 
